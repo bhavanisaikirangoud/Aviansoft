62f157528c43b3fbd3f96a82bd8df59821ea3051
bhavanisaikirangoud <react.saikirangoud@gmail.com>
2025-01-03 19:28:58 +0530

    stylings

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../index.css"
 
 const HomePage = () => {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to Task Manager</h1>
+      <h1 className="task-title"><i><b>Welcome to Task Manager</b></i></h1>
       <Link to="/tasks">
-        <button>Go to Task Manager</button>
+        <button className="task-button">Go to Task Manager</button>
       </Link>
     </div>
   );
